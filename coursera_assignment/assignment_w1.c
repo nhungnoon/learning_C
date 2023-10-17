9b11e1598df9a6491df35f8ec994b513473dbb06
nhungnoon <43654499+nhungnoon@users.noreply.github.com>
2023-10-16 22:10:02 -0400

    Create assignment_w1.c

---
+++ coursera_assignment/assignment_w1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Define typedef struct
+struct _retire_info {
+  int months;
+  double contribution;
+  double rate_of_return;
+};
+typedef struct _retire_info retire_info;
